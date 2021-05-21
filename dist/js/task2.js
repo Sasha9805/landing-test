dfe18626cb399a4187e31a2f8359164cdd8b3dfc
Sasha9805 <pimenovs33@gmail.com>
2021-05-21 19:51:22 +0300

    Добавлена 2-ая реализация функции drawRating

--- dist/js/task2.js
+++ dist/js/task2.js
@@ -1,19 +1,41 @@
 "use strict";
 
+// 1-й вариант - цикл и операция деления
 function drawRating(vote) {
 
   if (vote < 0 || vote > 100) {
-    return;
+    return 'no rating';
   }
 
   let str = '';
+  const resOfDivide = vote / 20;
   for (let i = 0; i < 5; i++) {
     // vote === i && vote === 0 - условие для нуля
-    str += vote / 20 > i || vote === i && vote === 0 ? '★' : '☆';
+    str += resOfDivide > i || vote === i && vote === 0 ? '★' : '☆';
   }
 
   return str;
 }
+
+// 2-ой вариант со switch/case
+// function drawRating(vote) {
+
+//   switch (true) {
+//     case vote >= 0 && vote <= 20:
+//       return '★☆☆☆☆';
+//     case vote > 20 && vote <= 40:
+//       return '★★☆☆☆';
+//     case vote > 40 && vote <= 60:
+//       return '★★★☆☆';
+//     case vote > 60 && vote <= 80:
+//       return '★★★★☆';
+//     case vote > 80 && vote <= 100:
+//       return '★★★★★';
+//     default:
+//       return 'no rating';
+//   }
+
+// }
 
 // Проверка работы результата
 for (let i = 0; i <= 100; i++) {
