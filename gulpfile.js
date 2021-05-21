d8979178c0856c5b2c9b7c2f953758d0607c9b18
Sasha9805 <pimenovs33@gmail.com>
2021-05-21 17:54:46 +0300

    Добавлен gulp, создана папка dist

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+const { src, dest, parallel } = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
+
+function minifyCSS() {
+  return src('src/assets//css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie7'}))
+    .pipe(dest('dist/assets/css'));
+}
+
+function transferFonts() {
+  return src('src/assets//fonts/**/*')
+    .pipe(dest('dist/assets/fonts'));
+}
+
+function transferJS() {
+  return src('src/js/main.js')
+    .pipe(dest('dist/js/'));
+}
+
+function minifyHTML() {
+  return src('src/index.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('dist/'));
+}
+
+function compressImages() {
+  return src('src/assets/img/**/*.{png,jpg,jpeg}')
+    .pipe(tinypng({
+      key: 'V6lb5vfCDdh7L8097LDtqv4cvP5Y2XJw'
+    }))
+    .pipe(dest('dist/assets/images/'));
+}
+
+exports.build = parallel(minifyHTML, minifyCSS, compressImages, transferFonts);
